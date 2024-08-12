e18baf22ef936cf654e3ca5e2c02faaef6d50cab
yurytrifonov <jutrifonov@uchicago.edu>
2024-08-12 10:54:22 -0500

    testing help

--- src/sreg/__init__.py
+++ src/sreg/__init__.py
@@ -1,5 +1,11 @@
 # sreg.py/__init__.py
 print("sreg package is being imported")
+"""
+Your Package
+============
+
+This package provides tools for performing XYZ operations. It includes modules for A, B, and C.
+"""
 
 # Import only the public functions and classes
 from .core import sreg, sreg_rgen
